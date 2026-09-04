d0741d832993b987e7533a049d9093f655de9ef5
samratvsn <samratvsn@gmail.com>
2026-09-04 13:40:01 +0545

    Initial Structure

---
+++ app/src/main/java/com/example/avalokan/ui/home/HomeScreenViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.avalokan.ui.home
+
+class HomeScreenViewModel {
+}
---
+++ app/src/main/java/com/example/avalokan/ui/navigation/NavDestination.kt
@@ -0,0 +1,6 @@
+package com.example.avalokan.ui.navigation
+
+interface NavDestination {
+    val route: String
+    val titleRes: Int
+}
